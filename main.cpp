ee73249865a9ebc61d226abb210eae0fc8a5f199
barath <82313227+Barath-2001@users.noreply.github.com>
2023-10-19 19:37:21 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -160,11 +160,11 @@ days = d;
 void SetData2(double h) {
 hours = h;
 }
-double Cal(cars& obj) {
+double Cal(const cars& obj) {
 total = obj.ppd() * days;
 return total;
 }
-double Cal2(cars& obj) {
+double Cal2(const cars& obj) {
 total = obj.pph() * hours;
 return total;
 }
@@ -663,7 +663,7 @@ cout << "\t\tEnter 4 for City 2017." << endl;
 cout << "\t\tEnter 5 to Exit." << endl;
 cout << endl;
 }
-void Show_Rent(cars & c, Person & p, TotalRent & r, int check) {
+void Show_Rent(const cars & c, Person & p, TotalRent & r, int check) {
 cout << "\n\t\t                       Car Rental - Customer Invoice                  " << endl;
 cout << "\t\t ///////////////////////////////////////////////////////////" << endl;
 cout << "\t\t | Invoice No. :" << "------------------|" << setw(18) << "#Cnb81353" << " |" << endl;
